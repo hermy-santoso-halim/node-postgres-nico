d17efcb571281c83769478bf8993ce8a2698ec34
hermy santoso halim <hermy.halim@prudential.co.id>
2019-11-03 01:02:32 +0700

    test

--- routes/pendingtrx.js
+++ routes/pendingtrx.js
@@ -17,7 +17,7 @@ router.use(cors({
 }));
 
 router.get('/echo', (httprequest, httpresponse) => {
-    httpresponse.status(300);
+    httpresponse.status(200);
     httpresponse.json({ success: true });
 })
 
