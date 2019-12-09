6d4b500cf4c109e6ec6869b0a7251563f817c611
hermy santoso halim <hermy.santoso@gmail.com>
2019-12-09 13:48:03 +0700

    update invoice

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.listen(port);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.disable('etag');
 
 app.use(logger('dev'));
 app.use(express.json());
